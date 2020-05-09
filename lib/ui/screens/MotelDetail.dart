40a2591a8789aa7853583ed03c653aa30d0b4361
Bryan Barrios <bryan.perez@est.uca.edu.ni>
2020-05-09 14:27:56 -0600

    Iniciacion

---
+++ lib/ui/screens/MotelDetail.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MotelDetailScreen extends StatelessWidget {
+  const MotelDetailScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Vista detalle de motel"),
+    );
+  }
+}
