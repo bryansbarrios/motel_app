3e0539cfc0d1a0378987bd1d5c384fad2a5ad2a8
Bryan Barrios <bryan.perez@est.uca.edu.ni>
2020-05-09 14:39:02 -0600

    .

---
+++ lib/ui/screens/MotelDetailScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:motel_app/core/models/Motel.dart';
+
+class MotelDetailScreen extends StatefulWidget {
+
+  @override
+  _MotelDetailScreenState createState() => _MotelDetailScreenState();
+}
+
+class _MotelDetailScreenState extends State<MotelDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              
+            ],
+          )
+        ],
+      )
+    );
+  }
+}
